ad3033694c64bda57201ea3832c8a88b16ec4fdb
maciej-mazur-github <maciej.mazur.mailbox@gmail.com>
2022-03-27 22:03:34 +0200

    Add isPalindrome method

--- src/NumberPalindrome.java
+++ src/NumberPalindrome.java
@@ -12,10 +12,20 @@ public class NumberPalindrome {
         return reversedNumber;
     }
 
+    public static boolean isPalindrome(int number) {
+        return number == reverseNumber(number);
+    }
+
     public static void main(String[] args) {
         System.out.println(reverseNumber(12345));
         System.out.println(reverseNumber(1212));
         System.out.println(reverseNumber(-486));
         System.out.println(reverseNumber(-4976));
+        System.out.println("****************************");
+        System.out.println(isPalindrome(12345));
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+        System.out.println(isPalindrome(11));
     }
 }
